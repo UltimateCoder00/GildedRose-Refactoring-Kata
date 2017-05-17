c71971a0c0ce8da372860a158d3fa9dd10f86ee2
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-05-17 17:25:58 +0100

    create change_quality method in quality class

--- lib/quality.rb
+++ lib/quality.rb
@@ -13,31 +13,31 @@ class Quality
         item.sell_in -= 1
       end
 
-      twice_decrease_speed = 1
-
-      if item.sell_in <= 0
-        twice_decrease_speed = 2
-      end
-
-      if item_is_conjured?(item.name)
-        item.quality = Conjured.new(item.quality).change_quality * twice_decrease_speed
-      elsif item_is_backstage_passed?(item.name)
-        item.quality = BackStagePasses.new(item.sell_in, item.quality).change_quality * twice_decrease_speed
-      elsif item_is_aged_brie?(item.name)
-        item.quality = AgedBrie.new(item.quality).change_quality
-      elsif item_is_sulfuras?(item.name)
-
-      else
-        item.quality -= (1 * twice_decrease_speed)
-      end
-
-      if item.quality > 50
-        item.quality = 50 unless item_is_sulfuras?(item.name)
-      end
+      item.quality = change_quality(item.name, item.sell_in, item.quality)
     end
   end
 
   private
+
+  def change_quality(name, sell_in, quality)
+    twice_decrease_speed = QUALITY_CHANGE
+
+    if sell_in <= 0
+      twice_decrease_speed = QUALITY_CHANGE * 2
+    end
+
+    if item_is_conjured?(name)
+      quality = Conjured.new(quality).change_quality * twice_decrease_speed
+    elsif item_is_backstage_passed?(name)
+      quality = BackStagePasses.new(sell_in, quality).change_quality * twice_decrease_speed
+    elsif item_is_aged_brie?(name)
+      quality = AgedBrie.new(quality).change_quality
+    elsif item_is_sulfuras?(name)
+      quality
+    else
+      quality -= (QUALITY_CHANGE * twice_decrease_speed)
+    end
+  end
 
   def item_is_sulfuras?(name)
     name == "Sulfuras"
